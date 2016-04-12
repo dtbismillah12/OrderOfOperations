89999db512999d3d75d82623f91e0f9497293b7e
dtbismillah12 <danyatazyeen12@augustana.edu>
2016-04-11 20:28:56 -0500

    More work on Animation Arena and MainActivity

--- app/src/main/java/com/example/danyatazyeen/orderofoperations/MainActivity.java
+++ app/src/main/java/com/example/danyatazyeen/orderofoperations/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ImageButton startBTN = (ImageButton) findViewById(R.id.startButton);
+
     }
 
 }
