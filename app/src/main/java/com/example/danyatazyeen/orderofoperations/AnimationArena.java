448d2042a41b265ab5ab9ec55c9760501378d033
dtbismillah12 <danyatazyeen12@augustana.edu>
2016-04-11 21:54:59 -0500

    set background to start screen to fill parent

--- app/src/main/java/com/example/danyatazyeen/orderofoperations/AnimationArena.java
+++ app/src/main/java/com/example/danyatazyeen/orderofoperations/AnimationArena.java
@@ -1,7 +1,30 @@
 package com.example.danyatazyeen.orderofoperations;
 
+import android.graphics.Canvas;
+
 /**
  * Created by danyatazyeen on 4/11/16.
  */
+
+import android.graphics.Canvas;
 public class AnimationArena {
+
+        private Spaceship ship;
+        private static int nextOpenSlot;
+
+        public AnimationArena () {
+            //INSTANTIATE THE BALL
+            ship = new Spaceship();
+        }
+
+        public void update (int width, int height) {
+            //ship.move(0, 0, width, height, allBalls);
+        }
+
+        public void draw (Canvas canvas) {
+            //WIPE THE CANVAS CLEAN
+            canvas.drawRGB(75, 0, 130);
+
+        }
+
 }
