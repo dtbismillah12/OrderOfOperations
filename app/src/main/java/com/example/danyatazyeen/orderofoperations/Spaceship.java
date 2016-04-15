4d801f2057b169afdd5c3a379009974f2429d4fd
dtbismillah12 <danyatazyeen12@augustana.edu>
2016-04-15 14:16:38 -0500

    Added more code into the constructor for the Spaceship class for the player. Referencing this open source code for SpaceInvaders to understand their structure. Ours will be different in the navigating and mathematical learning aspect, however. For the essential game though, it may be a good reference.

--- app/src/main/java/com/example/danyatazyeen/orderofoperations/Spaceship.java
+++ app/src/main/java/com/example/danyatazyeen/orderofoperations/Spaceship.java
@@ -1,7 +1,33 @@
 package com.example.danyatazyeen.orderofoperations;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.RectF;
+
 /**
  * Created by danyatazyeen on 4/6/16.
+ * referencing http://gamecodeschool.com/android/coding-a-space-invaders-game/
  */
 public class Spaceship {
+    RectF rectangle;
+    private Bitmap bitmap;
+    private float lengthShip;
+    private float xShipLeft;
+    private float heightShip;
+    private float yShipTop;
+
+    public Spaceship(Context context, int screenX, int screenY){
+        rectangle = new RectF();
+
+        lengthShip = screenX/10;
+        heightShip = screenY/10;
+
+        xShipLeft = screenX/2;
+        yShipTop = screenY-20;
+
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ship);
+        bitmap = Bitmap.createScaledBitmap(bitmap, (int) lengthShip, (int) heightShip, false);
+    }
+
 }
