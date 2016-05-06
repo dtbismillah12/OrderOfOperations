ee0a55c4716e8bf850cbe583208fc4a006f894e3
dtbismillah12 <danyatazyeen12@augustana.edu>
2016-05-06 15:05:29 -0500

    Got intent to ScoreScreen to launch!

--- app/src/main/java/edu/augustana/dreamteam/orderofoperations/gui/ScoreScreen.java
+++ app/src/main/java/edu/augustana/dreamteam/orderofoperations/gui/ScoreScreen.java
@@ -1,11 +1,14 @@
 package edu.augustana.dreamteam.orderofoperations.gui;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -15,34 +18,55 @@ import edu.augustana.dreamteam.orderofoperations.R;
 import java.util.Set;
 
 
-public class ScoreScreen extends AppCompatActivity {
+public class ScoreScreen extends Activity {
+    private SharedPreferences sharedPref;
+    private Set<String> topFiveScores;
+    private String[] topFive;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_score_screen);
-    }
-
-    SharedPreferences sharedPref;
-    Set<String> topFiveScores;
-    String[] topFive;
-
-    ImageView background;
-    ImageButton restartBTN;
-    ImageButton endBTN;
+    private ImageView background;
+    private ImageButton restartBTN;
+    private ImageButton endBTN;
 
     int finalScore;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_screen);
 
-    public ScoreScreen(){
+        // Get a Display object to access screen details
+        Display display = getWindowManager().getDefaultDisplay();
+
+
         background = (ImageView) findViewById(R.id.endView);
         restartBTN = (ImageButton) findViewById(R.id.restartButton);
         endBTN = (ImageButton) findViewById(R.id.endButton);
+
+
+        restartBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View btn) {
+
+            }
+
+        });
+
+        endBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View btn) {
+                System.exit(0);
+            }
+
+        });
     }
+
+
+
 
     //check if player's score is higher than the current highscore
     //if so, pop up window that notifies player
-    public void checkHighSchore(){
+    public void checkHighScore(){
         sharedPref = getSharedPreferences("HighScores", Context.MODE_PRIVATE);
         topFiveScores = sharedPref.getStringSet("topFive", null);
         topFive = topFiveScores.toArray(new String[topFiveScores.size()]);
@@ -55,12 +79,7 @@ public class ScoreScreen extends AppCompatActivity {
 
     }
 
-//    restartBTN.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View btn) {
-//        }
-//
-//    });
+
 
 
 }
