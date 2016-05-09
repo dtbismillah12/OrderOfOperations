63ca6828c9f28ba0399af4689a3ded5474ebe61b
dtbismillah12 <danyatazyeen12@augustana.edu>
2016-05-09 14:05:07 -0500

    getting high scores set up on score screen

--- app/src/main/java/edu/augustana/dreamteam/orderofoperations/gui/ScoreScreen.java
+++ app/src/main/java/edu/augustana/dreamteam/orderofoperations/gui/ScoreScreen.java
@@ -9,11 +9,15 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Display;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import edu.augustana.dreamteam.orderofoperations.R;
 
@@ -21,7 +25,7 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 
-public class ScoreScreen extends Activity {
+public class ScoreScreen extends Activity{
     private SharedPreferences sharedPref;
     private Set<String> topFiveScores;
     private String[] topFive;
@@ -30,7 +34,12 @@ public class ScoreScreen extends Activity {
     private ImageButton endBTN;
     private Canvas canvas;
     private Paint paint;
+    FrameLayout endScreen;
     private SurfaceHolder holder;
+
+    private TextView highScore1;
+    private TextView highScore2;
+    private TextView highScore3;
 
     int finalScore;
 
@@ -43,10 +52,19 @@ public class ScoreScreen extends Activity {
         // Get a Display object to access screen details
         Display display = getWindowManager().getDefaultDisplay();
 
+//        SurfaceView surfView = (SurfaceView) findViewById(R.id.surfView);
+//        holder = surfView.getHolder();
+//        holder.addCallback(this);
 
+        endScreen = (FrameLayout) findViewById(R.id.scoreView);
         background = (ImageView) findViewById(R.id.endView);
         restartBTN = (ImageButton) findViewById(R.id.restartButton);
-        //endBTN = (ImageButton) findViewById(R.id.endButton);
+
+        highScore1 = (TextView) findViewById(R.id.topScore1);
+        highScore2 = (TextView) findViewById(R.id.topScore2);
+        highScore3 = (TextView) findViewById(R.id.topScore3);
+
+        displayHighScores();
 
 
         restartBTN.setOnClickListener(new View.OnClickListener() {
@@ -59,14 +77,51 @@ public class ScoreScreen extends Activity {
             }
 
         });
-//
-//        endBTN.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View btn) {
-//                System.exit(0);
-//            }
-//
-//        });
+    }
+
+
+    private void draw(){
+        // Make sure our drawing surface is valid or we crash
+//        if (holder.getSurface().isValid()) {
+//            // Lock the canvas ready to draw
+//            canvas = holder.lockCanvas();
+
+            Intent scoreIntent = getIntent();
+            int finScore = scoreIntent.getIntExtra("score", 0);
+            int screenHeight = scoreIntent.getIntExtra("screenHeight", 0);
+
+            String strTopScores = sharedPref.getString("topThree", "");
+            StringTokenizer st = new StringTokenizer(strTopScores, ",");
+            int[] highScores = new int[3];
+            for (int i = 0; i < highScores.length; i++) {
+                highScores[i] = Integer.parseInt(st.nextToken());
+            }
+
+            //Draw high scores in middle of screen
+
+
+
+
+//            paint.setTextSize(40);
+//            paint.setColor(Color.argb(255, 249, 129, 0));
+//            canvas.drawText("Top Score 1", 30, screenHeight - 150, paint);
+//            canvas.drawText("Top Score 2", 30, screenHeight - 200, paint);
+//            canvas.drawText("Top Score 3", 30, screenHeight - 250, paint);
+
+//            canvas.drawText("Score: " + finScore, 5, screenHeight - 40, paint);
+//            // Draw everything to the screen
+//            holder.unlockCanvasAndPost(canvas);
+//            endScreen.draw(canvas);
+
+       // }
+    }
+    /*
+    Draw High Scores to screen.
+     */
+    public void displayHighScores(){
+        highScore1.setText("Top Score 1");
+        highScore1.setText("Top Score 2");
+        highScore1.setText("Top Score 3");
     }
 
 
@@ -84,38 +139,6 @@ public class ScoreScreen extends Activity {
         }
 
     }
-
-    private void draw(){
-        // Make sure our drawing surface is valid or we crash
-        if (holder.getSurface().isValid()) {
-            // Lock the canvas ready to draw
-            canvas = holder.lockCanvas();
-
-            Intent scoreIntent = getIntent();
-            int finScore = scoreIntent.getIntExtra("score", 0);
-            int screenHeight = scoreIntent.getIntExtra("screenHeight", 0);
-
-            String strTopScores = sharedPref.getString("topThree", "");
-            StringTokenizer st = new StringTokenizer(strTopScores, ",");
-            int[] highScores = new int[3];
-            for (int i = 0; i < highScores.length; i++) {
-                highScores[i] = Integer.parseInt(st.nextToken());
-            }
-
-            //Draw high scores in middle of screen
-            paint.setTextSize(40);
-            paint.setColor(Color.argb(255, 255, 255, 255));
-            canvas.drawText("Top Score 1", 10, screenHeight - 150, paint);
-            canvas.drawText("Top Score 2", 10, screenHeight - 200, paint);
-            canvas.drawText("Top Score 3", 10, screenHeight - 250, paint);
-
-            canvas.drawText("Score: " + finScore, 5, screenHeight - 40, paint);
-            // Draw everything to the screen
-            holder.unlockCanvasAndPost(canvas);
-        }
-    }
-
-
 
 
 }
